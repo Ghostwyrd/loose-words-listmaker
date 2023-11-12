77f9eed787453809f0e174102e58ab0f0813bf23
Ghost <26860711+Ghostwyrd@users.noreply.github.com>
2023-11-12 08:44:06 -0800

    removed 'loose words' program stub, stopped tracking a_list.txt and b_list.txt

--- List-condenser.py
+++ List-condenser.py
@@ -1,39 +1,40 @@
 #program to remove white spaces from a text list and 
 #return a clean txt file w/ one word per new line.
+#adj = " "
+#adj_list = []
 
-def pagereader(pagelist_of_terms):
-    f = open(pagelist_of_terms, "r")
-    adj = f.read()
-    f.close()
+#def pagereader("adjective_list.txt"):
+f = open("adjective_list.txt", "r")
+adj = f.read()
+f.close()
 
 #pagereader("adjective_list.txt")
 
-def pagemaker():
-    adj = adj.title()
-    adj_list = adj.split()
+adj = adj.title()
+adj_list = adj.split()
     #print(adj_list)
     #print line for troubleshooting steps only
 
-    adj_set = set(adj_list)
+adj_set = set(adj_list)
     #print(adj_set)
     #print line for troubleshooting steps only
 
-    adj_list = sorted(adj_set)
+adj_list = sorted(adj_set)
     #print(adj_list)
     #print('\n'.join(adj_list))
     #print line for troubleshooting steps only
 
 
-def pagewriter(pagelist_of_terms):
-    f = open(pagelist_of_terms, "w")
+def pagewriter(page_name):
+    f = open(page_name, "w")
     f.write('\n'.join(adj_list))
     f.write('\n')
     f.close()
 
-pagereader("adjective_list.txt")
-pagemaker()
+#pagereader("adjective_list.txt")
+#pagemaker()
 pagewriter("a_list.txt")
 
-pagereader("animal_list.txt")
-pagemaker()
-pagewriter("b_list.txt")
+#pagereader("animal_list.txt")
+#pagemaker()
+#pagewriter("b_list.txt")
--- loose-words-listmaker.py
+++
@@ -1,6 +0,0 @@
-# a program to convert a text list of words into a python list
-#f = open("adjectiveList.txt",'r')
-#print(f.read())
-import os
-print(os.getcwd())
-
